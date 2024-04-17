2817e5920af6ba2700ab3f4d7325ab6aff77b5f3
BrianHer182 <233289@ids.upchiapas.edu.mx>
2024-04-16 23:39:45 -0600

    Primer commit

---
+++ data/story.js
@@ -0,0 +1,634 @@
+var story = {
+ "docName": "Untitled",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "Untitled",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "53:4",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 3",
+   "image": "surface-pro-4-3.png",
+   "index": 0,
+   "width": 912,
+   "height": 1368,
+   "x": -3700,
+   "y": -2196,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group 7",
+     "rect": {
+      "x": 161,
+      "y": 470,
+      "width": 639,
+      "height": 442
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Surface Pro 4 - 3",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 912,
+      "height": 1368
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "75:36",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 5",
+   "image": "surface-pro-4-5.png",
+   "index": 1,
+   "width": 912,
+   "height": 1368,
+   "x": -2179,
+   "y": -191,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 45",
+     "rect": {
+      "x": 456,
+      "y": 0,
+      "width": 456,
+      "height": 1368
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "2:4",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 1",
+   "image": "surface-pro-4-1.png",
+   "index": 2,
+   "width": 912,
+   "height": 1368,
+   "x": -2174,
+   "y": -2196,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Crear Cuenta",
+     "rect": {
+      "x": 354,
+      "y": 804,
+      "width": 233,
+      "height": 68
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Rectangle 20",
+     "rect": {
+      "x": 287,
+      "y": 892,
+      "width": 340,
+      "height": 70
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "108:108",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 9",
+   "image": "surface-pro-4-9.png",
+   "index": 3,
+   "width": 912,
+   "height": 1368,
+   "x": -858,
+   "y": 1869,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Arrow 1",
+     "rect": {
+      "x": 56.58941650390625,
+      "y": 58.911376953125,
+      "width": 49.598270416259766,
+      "height": 41.98324203491211
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "Arrow 2",
+     "rect": {
+      "x": 909.25927734375,
+      "y": 1917.3837890625,
+      "width": 58.25929641723633,
+      "height": 66.3837890625
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "85:12",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 6",
+   "image": "surface-pro-4-6.png",
+   "index": 4,
+   "width": 912,
+   "height": 1368,
+   "x": -797,
+   "y": -191,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 46",
+     "rect": {
+      "x": 464,
+      "y": 0,
+      "width": 456,
+      "height": 1368
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Rectangle 47",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 456,
+      "height": 1368
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "75:8",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 4",
+   "image": "surface-pro-4-4.png",
+   "index": 5,
+   "width": 912,
+   "height": 1368,
+   "x": -625,
+   "y": -2215,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Inicia Sesion",
+     "rect": {
+      "x": 511,
+      "y": 1185,
+      "width": 121,
+      "height": 47
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    },
+    {
+     "name": "Group 1",
+     "rect": {
+      "x": 294,
+      "y": 1072,
+      "width": 333,
+      "height": 99
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "89:26",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 7",
+   "image": "surface-pro-4-7.png",
+   "index": 6,
+   "width": 912,
+   "height": 1368,
+   "x": 685,
+   "y": -191,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 20",
+     "rect": {
+      "x": 675,
+      "y": 1276,
+      "width": 221,
+      "height": 59
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 7,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "89:47",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 8",
+   "image": "surface-pro-4-8.png",
+   "index": 7,
+   "width": 912,
+   "height": 1368,
+   "x": 685,
+   "y": 1869,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 32",
+     "rect": {
+      "x": 644.968505859375,
+      "y": 1328,
+      "width": 555.0629272460938,
+      "height": 641
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 9,
+       "transAnimType": 2,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "salud1-300x223 1",
+     "rect": {
+      "x": 658.112060546875,
+      "y": 1339,
+      "width": 521.698486328125,
+      "height": 353
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 9,
+       "transAnimType": 2,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "Group 5",
+     "rect": {
+      "x": 7,
+      "y": 1328,
+      "width": 555.0629272460938,
+      "height": 641
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "Group 4",
+     "rect": {
+      "x": -17,
+      "y": -36,
+      "width": 929.1491088867188,
+      "height": 2924
+     },
+     "index": 15,
+     "reactions": []
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "173:19",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 13",
+   "image": "surface-pro-4-13.png",
+   "index": 8,
+   "width": 912,
+   "height": 1368,
+   "x": 2228,
+   "y": -67,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "167:2",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 10",
+   "image": "surface-pro-4-10.png",
+   "index": 9,
+   "width": 912,
+   "height": 1368,
+   "x": 2228,
+   "y": 1869,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group 9",
+     "rect": {
+      "x": 54,
+      "y": 558,
+      "width": 222,
+      "height": 310
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 10,
+       "transAnimType": 2,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "Group 10",
+     "rect": {
+      "x": 345,
+      "y": 558,
+      "width": 222,
+      "height": 318
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 11,
+       "transAnimType": 2,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "Group 11",
+     "rect": {
+      "x": 636,
+      "y": 558,
+      "width": 222,
+      "height": 310
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 9,
+       "frameIndex": 8,
+       "transAnimType": 2,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "173:14",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 11",
+   "image": "surface-pro-4-11.png",
+   "index": 10,
+   "width": 912,
+   "height": 1368,
+   "x": 2228,
+   "y": 3630,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "173:16",
+   "groupIndex": 0,
+   "title": "Surface Pro 4 - 12",
+   "image": "surface-pro-4-12.png",
+   "index": 11,
+   "width": 912,
+   "height": 1368,
+   "x": 3834,
+   "y": 1869,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "startPageIndex": 2,
+ "totalImages": 12,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
